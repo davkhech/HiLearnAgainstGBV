976f07457ae11cd43a77792d1c3a0cf73331c80d
Gevorg Martirosyan <gevorgmartirosyan97@gmail.com>
2018-12-16 00:37:36 +0400

    Comment view implemented

--- app/src/main/java/com/example/davkhech/hilearnagainstgbv/MainActivity.java
+++ app/src/main/java/com/example/davkhech/hilearnagainstgbv/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.davkhech.hilearnagainstgbv;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +47,35 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        populateSpinner(R.id.type_spinner, R.array.violence_type_array);
+    }
+
+    private void populateSpinner (int spinnerId, int arrayId) {
+        Spinner spinner = (Spinner) findViewById(spinnerId);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                arrayId, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    public void onClick(View b) {
+        Spinner violenceSpinner = findViewById(R.id.type_spinner);
+        String violenceType = violenceSpinner.getSelectedItem().toString();
+
+        RadioGroup radioGroup = findViewById(R.id.dissatisfaction_radio_group);
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        Button selectedButton = findViewById(selectedId);
+        String satisfactionLevel = selectedButton.getText().toString();
+
+        EditText commentTextView = findViewById(R.id.comment_text_field);
+        String commentText = commentTextView.getText().toString();
+        
+        // send to map here!
     }
 
 }
